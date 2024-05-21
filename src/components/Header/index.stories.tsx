78db418e9e29cc59416e16258e54b4b1a92227f3
gjaegyun <jaegyungim06@gmail.com>
2024-05-21 14:48:06 +0900

    Add settings

---
+++ src/components/Header/index.stories.tsx
@@ -0,0 +1,20 @@
+import Header from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Header> = {
+  component: Header,
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Primary: Story = {
+  args: {
+    currentTheme: 1,
+  },
+};
